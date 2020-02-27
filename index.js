5606f6f573b1fcd8495d4b5841624a7846277175
Rachele Edwards <dot.com1472@gmail.com>
2020-02-27 13:25:08 -0700

    did task 1 and most of 2 pretty easily--needing to do milesPerGallon calculation to do remaining car problems..hmm

--- index.js
+++ index.js
@@ -29,37 +29,64 @@ class Airplane {
 
 /*
   TASK 1
-    - Write a Person class whose constructor initializes `name` and `age` from arguments.
-    - All instances of Person should also initialize with an empty `stomach` array.
-    - Give instances of Person the ability to `.eat("someFood")`:
-        + When eating an edible, it should be pushed into the `stomach`.
-        + The `eat` method should have no effect if there are 10 items in the `stomach`.
-    - Give instances of Person the ability to `.poop()`:
-        + When an instance poops, its `stomach` should empty.
-    - Give instances of Person a method `.toString()`:
+    - Write a Person class whose constructor initializes `name` and `age` from arguments.✅
+    - All instances of Person should also initialize with an empty `stomach` array.✅
+    - Give instances of Person the ability to `.eat("someFood")`:✅
+        + When eating an edible, it should be pushed into the `stomach`.✅
+        + The `eat` method should have no effect if there are 10 items in the `stomach`.✅
+    - Give instances of Person the ability to `.poop()`:✅
+        + When an instance poops, its `stomach` should empty.✅
+    - Give instances of Person a method `.toString()`:✅
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood){
+    if (this.stomach.length < 10){
+      return this.stomach.push(this.stomach);
+    }
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
   TASK 2
-    - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
+    - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.✅
     - All instances built with Car:
-        + should initialize with a `tank` at 0
-        + should initialize with an `odometer` at 0
-    - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
-    - Give cars ability to `.drive(distance)`. The distance driven:
-        + Should cause the `odometer` to go up.
+        + should initialize with a `tank` at 0 ✅
+        + should initialize with an `odometer` at 0 ✅
+    - Give cars the ability to get fueled with a `.fill(gallons)` method. ✅ Add the gallons to `tank`.✅
+    - Give cars ability to `.drive(distance)`. ✅ The distance driven:
+        + Should cause the `odometer` to go up.✅
         + Should cause the the `tank` to go down taking `milesPerGallon` into account.
     - A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank = this.tank + gallons;
+  }
+  drive(distance){
+    this.odometer = this.odometer + distance;
+    this.tank = 
+  }
 }
 
 /*
